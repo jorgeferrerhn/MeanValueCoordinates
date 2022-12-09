ec41b9ca219e6e41fb1c003213cc86bf91240c23
Jorge Ferrer <ferrerhernaez.2053116@studenti.uniroma1.it>
2022-12-09 23:41:54 +0100

    new files

--- algorithm.py
+++ algorithm.py
@@ -3,12 +3,22 @@ import numpy as np
 
 
 #Source
-img = cv.imread('img/test1.jpg')
+source = cv.imread('img/polar_bear.png')
+sourcegray = cv.cvtColor(source, cv.COLOR_BGR2GRAY)
+ret, thresh = cv.threshold(sourcegray, 127, 255, 0)
+contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
 #Target
-px = img.shape
+#target = cv.imread('img/black_beach.jpg')
+
+#Draw the contours
+cv.drawContours(source, contours, -1, (0,255,0), 3)
+
+
+px = source.shape
+
 
 print(px)
 
-cv.imshow("Image",img)
+cv.imshow("Image",source)
 cv.waitKey(0)
