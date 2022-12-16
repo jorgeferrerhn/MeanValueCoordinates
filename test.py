a7e43738be9104c30be6d6a879d2b4aaa19b9d1f
AdanFormisano <adan.formisano@gmail.com>
2022-12-16 12:17:44 +0100

    Corrected comments and border detection.

--- test.py
+++ test.py
@@ -1,32 +1,35 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('source_03_edge.png')
+img = cv.imread('source_02_edge.png')
 edges = set()
 z = np.array([0,0,0]) # np.array.all() ritorna false se c'e' almeno un zero
 
 
 print("Image shape: ",str(img.shape))
-# Ciclo sulle righe, cerca primo pixel colorato e lo salva come edge.
-# Quando finisce "zona colorata" trovando primo pixel (0,0,0), salva il pixel precendete come edge.
+# Iterazione su ogni pixel
 i = 0
 for row in img:
     j = 0
     for px in row:
-        # Se pixel e' colorato salvare come edge => inizia "zona colorata"
+        # Se il pixel è colorato, controlla tutti i suoi adiacenti
         if not (px == z).all():
-            # print("Pixel: ",i,j)
+
+            # Coordinate dei pixel adiacenti
             coords = [(i-1,j),(i+1,j),(i,j-1),(i,j+1)]
 
-            for coord in coords:
-                valid = (coord[0] in range(1,img.shape[0])) and (coord[1] in range(1,img.shape[1]))
+            # Per ogni adiacente controlla prima se appartiene al bordo del file
+            for adiacentCoord in coords:
+                notBorder = (adiacentCoord[0] in range(0,img.shape[0])) and (adiacentCoord[1] in range(0,img.shape[1]))
                 
-                if valid:
-                    if (img[coord[0]][coord[1]] == z).all(): #vicino è non-colorato --> boundary
-                        print(coord[0],coord[1], px)
-                        # Salva come edge
-                        edges.add((i,j))
-                elif ((coord[0] == 0 or coord[1] == 0 ) or (coord[0] == img.shape[0] or coord[1] == img.shape[1])):
+                # Se non appartiene al bordo e il pixel adiacente è [0,0,0] aggiungi agli edges
+                if notBorder and (img[adiacentCoord[0]][adiacentCoord[1]] == z).all():
+                    print("Pixel: ",i,j,"| Empty adiacent: ",adiacentCoord[0],adiacentCoord[1])
+                    # Salva come edge
+                    edges.add((i,j))
+
+                # Se il pixel appartiene al bordo aggiungi automaticamente (abbiamo già controllato che il pixel è colorato)
+                elif ((adiacentCoord[0] == -1 or adiacentCoord[1] == -1 ) or (adiacentCoord[0] == img.shape[0] or adiacentCoord[1] == img.shape[1])):
                     edges.add((i,j))
         j+=1
     i+=1
