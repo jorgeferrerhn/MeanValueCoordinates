a72cdc9dfc88bc4c83cd2fe008f6b2b46bcbfbb6
AdanFormisano <formisano.1813917@studenti.uniromai.it>
2022-12-20 14:31:36 +0100

    MVC formula, better edge rappresentation - TODO "Buffer" per gli angoli: Ogni angolo puó essere calcolato una volta e poi essere salvato in un buffer - TODO Provare ad utilizzare Numpy per la distanza di 2 punti.
    Added target image.
    
    Co-authored-by: TheRory <TheRory@users.noreply.github.com>
    Co-authored-by: jorgeferrerhn <jorgeferrerhn@users.noreply.github.com>

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import math
 
 img = cv.imread('source_02_edge.png')
 z = np.array([0,0,0]) # np.array.all() ritorna false se c'e' almeno un zero
@@ -8,73 +9,101 @@ z = np.array([0,0,0]) # np.array.all() ritorna false se c'e' almeno un zero
 def BuildEdge():
     edges = set()
     # Iterazione su ogni pixel
-    i = 0
+    Y = 0
     for row in img:
-        j = 0
+        X = 0
         for px in row:
             # Se il pixel è colorato, controlla tutti i suoi adiacenti
             if not (px == z).all():
 
                 # Coordinate dei pixel adiacenti
-                coords = [(i-1,j),(i+1,j),(i,j-1),(i,j+1)]
+                coords = [(Y-1,X),(Y+1,X),(Y,X-1),(Y,X+1)]
 
                 # Per ogni adiacente controlla prima se appartiene al bordo del file
                 for adiacentCoord in coords:
-                    notBorder = (adiacentCoord[0] in range(0,img.shape[0])) and (adiacentCoord[1] in range(0,img.shape[1]))
+                    adiacentY = adiacentCoord[0]
+                    adiacentX = adiacentCoord[1]
+                    notBorder = (adiacentY in range(0,img.shape[0])) and (adiacentX in range(0,img.shape[1]))
                     
                     # Se non appartiene al bordo e il pixel adiacente è [0,0,0] aggiungi agli edges
-                    if notBorder and (img[adiacentCoord[0]][adiacentCoord[1]] == z).all():
-                        # print("Pixel: ",i,j,"| Empty adiacent: ",adiacentCoord[0],adiacentCoord[1])
+                    if notBorder and (img[adiacentY][adiacentX] == z).all():
+                        # print("Pixel: ",Y,X,"| Empty adiacent: ",adiacentCoord[0],adiacentCoord[1])
                         # Salva come edge
-                        edges.add((i,j))
+                        edges.add((Y,X))
 
                     # Se il pixel appartiene al bordo aggiungi automaticamente (abbiamo già controllato che il pixel è colorato)
-                    elif ((adiacentCoord[0] == -1 or adiacentCoord[1] == -1 ) or (adiacentCoord[0] == img.shape[0] or adiacentCoord[1] == img.shape[1])):
-                        edges.add((i,j))
-            j+=1
-        i+=1
-    # Ritorno le coordinate dei pixel dell'edge
+                    elif ((adiacentY == -1 or adiacentX == -1 ) or (adiacentY == img.shape[0] or adiacentX == img.shape[1])):
+                        edges.add((Y,X))
+            X+=1
+        Y+=1
+        
     return edges
 
-def BuildMVC(edges, px):
+# Funzione che ritorna un dizionario contenente per ogni px dell'edge, i suoi px adiacenti
+def BuildEdgeAdiacent(listEdges):
+    Edges = dict()
+    for e in listEdges:
+        Edges[e] = []
+        Y,X = e[0], e[1]
+        adiacentCoords = [(Y-1,X), (Y,X-1), (Y+1,X), (Y,X+1)]
+        # Per i pixel direttamente adiacenti controlla se fanno parte di listEdges
+        for adiacent in adiacentCoords:
+            if adiacent in listEdges:
+                Edges[e].append(adiacent)
+        if len(Edges[e]) < 2:
+            exCoords = [(Y-1, X+1), (Y-1, X-1), (Y+1, X-1), (Y+1, X+1)]
+
+            for exC in exCoords:
+                if exC in listEdges and len(Edges[e]) < 2:
+                    Edges[e].append(adiacent) 
+
+    return Edges
+
+# Funzione che ritorna una lista delle lamda calcolate per px su ogni pixel dell'edge
+def BuildMVC(Edges, px):
     # Lista contenente tutti i lamda del targetPx
     myLamdas = []
     
-    '''
-    for p in edges:
-        # Trovare pixel adiacente che appartiene all'edge
+    for edge in Edges:
+        # Gli angoli vengono usati tutti 2 volte max, quindi possiamo fare un buffer in cui salviamo gli angoli che devono essere ancora utilizzati per la seconda volta
+        # TODO provare ad implementare un "buffer" per gli angoli
+        p = edge
+        p_1 = Edges[edge][0]
+        p1 = Edges[edge][1]
 
-        print("P-1: ", p_1, "P1: ", p1)
-    '''
+        # Angoli usati nella formula di wi
+        a1 = (np.arctan2(px[0] - p1[0], px[1] - p1[1])) - (np.arctan2(px[0] - p[0], px[1] - p[1]))
+        a_1 = (np.arctan2(px[0] - p[0], px[1] - p[1])) - (np.arctan2(px[0] - p_1[0], px[1] - p_1[1]))
 
-        # lamda_edge(px) = w_i/1 (?) dove w_i = tan()
-        # a1
-        # a_1 = (np.arctan2(px[0] - p[0], px[1] - p[1])) - (np.arctan2(px[0] - ))
-    return
+        # TODO Provare ad usare Numpy per la distanza
+        wi = (np.tan(a_1/2) + np.tan(a1/2))/math.dist(p, px)
+        myLamdas.append(wi)
 
-Edges = list(BuildEdge())
-print("Unsorted: ", Edges)
-# Bisogna vedere come ordinare la lista
-Edges.sort()
-print("Sorted: ",Edges)
+    print(p, ":", myLamdas)
 
-'''
-y = 0
+    return myLamdas
+
+
+listEdges = list(BuildEdge())
+listEdges.sort()
+Px = dict()
+dictEdges = BuildEdgeAdiacent(listEdges)
+
+Y = 0
 for row in img:
-    x = 0
+    X = 0
     for px in row:
         # Se pixel é colorato
         if not (px == z).all():
-            # Se pixel non é nella lista degli edge
-            if not (y,x) in Edges:
+            # Se pixel non é nella negli edge
+            if not (Y,X) in dictEdges:
                 # Costruisco l'MVC per ogni pixel dell'area da copiare (interna all'edge)
-                BuildMVC(Edges, (y,x))
-        x+=1
-    y+=1
-'''
+                Px[(Y,X)] = BuildMVC(dictEdges, (Y,X))
+        X+=1
+    Y+=1
 
 copyImage = img.copy()
-for e in Edges:
+for e in listEdges:
     copyImage[e[0]][e[1]] = (102, 255, 102)
 
 cv.imshow("Copy Image", copyImage)
